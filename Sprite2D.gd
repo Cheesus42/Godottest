d4d936e1d4c324703fd317460f0da206be8886a1
Leon Stumpf <stumpfleon@gmx.de>
2024-04-22 20:13:11 +0200

    Bettter movementA

--- Sprite2D.gd
+++ Sprite2D.gd
@@ -1,13 +1,11 @@
 extends Sprite2D
 
 var speed = 400
-var angular_speed = PI
 
 	
 func _process(delta):
-	rotation += angular_speed * delta
 	var direction = Vector2.ZERO
-	var velocity = Vector2.ZERO
+	
 	if Input.is_action_pressed("ui_up"):
 		direction.y = -1
 	if Input.is_action_pressed("ui_down"):
@@ -17,7 +15,6 @@ func _process(delta):
 	if Input.is_action_pressed("ui_right"):
 		direction.x = 1
 	position += direction * delta * speed
-
 
 func _on_button_pressed():
 	set_process(not is_processing())
